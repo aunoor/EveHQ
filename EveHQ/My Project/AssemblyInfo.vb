0554e783778060b8210b0f158d592964f2600c53
Mike Eshva <mike@eshva.ru>
2017-07-29 21:15:58 +0300

    2.31.2 release version numbers

--- EveHQ/My Project/AssemblyInfo.vb
+++ EveHQ/My Project/AssemblyInfo.vb
@@ -13,7 +13,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyCompany("EveHQ team")> 
 <Assembly: AssemblyProduct("EveHQ")> 
 <Assembly: AssemblyCopyright("")>
-<Assembly: AssemblyTrademark("2017-07-28")>
+<Assembly: AssemblyTrademark("2017-07-29")>
 
 <Assembly: ComVisible(False)> 
 
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("2.31.1.0")>
-<Assembly: AssemblyFileVersion("2.31.1.0")>
+<Assembly: AssemblyVersion("2.31.2.0")>
+<Assembly: AssemblyFileVersion("2.31.2.0")>
