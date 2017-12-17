8c301de9b96c154a39aedc16d1b338cede787b62
Mike Eshva <mike@eshva.ru>
2017-12-18 00:42:20 +0300

    Removed unnecessary files.

--- EveHQ.Prism/Classes/AssetLocation.vb
+++
@@ -1,9 +0,0 @@
-Imports EveHQ.EveData
-
-Namespace Classes
-	Public NotInheritable Class AssetLocation1
-		Public SolarSystem As SolarSystem
-		Public ContainerId As Long
-		Public ContainerName As String
-	End Class
-End NameSpace
